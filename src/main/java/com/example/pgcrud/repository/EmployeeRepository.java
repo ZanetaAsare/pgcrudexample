8fd3567829e429aa36d32679a1795b3323aa070b
Zaneta Asare <asarezaneta@gmail.com>
2021-04-07 09:21:38 +0000

    postgres crud ex

---
+++ src/main/java/com/example/pgcrud/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.pgcrud.repository;
+
+import com.example.pgcrud.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
